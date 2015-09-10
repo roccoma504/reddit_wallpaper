5995827f5e03d884102f69a1c250d5bb1b4f9e91
roccoma <roccoma504@gmail.com>
2015-09-10 19:05:17 -0400

    Removed shell implementation
    Swapped out shell implementation for NSScreen implementation.

--- README.md
+++
@@ -1,8 +0,0 @@
-# reddit_wallpaper
-Python script to set an OS X wallpaper to the top post of WQHD_Wallpaper
-
-To run from terminal use the following 
-
-python /filepath/reddit_mac_wallpaper.py
-
-You will need to install python 2.7 and PRAW
--- reddit_mac_wallpaper.py
+++ reddit_mac_wallpaper.py
@@ -1,35 +1,30 @@
-__author__ = 'rocco'
+#!/usr/bin/python
 
+# This script changes the wallpaper of the current OSX desktop. It will change the wallpaper of the desktop on each screen but not each desktop.
+
+from AppKit import NSWorkspace, NSScreen
+from Foundation import NSURL
 import os
 import praw
-import subprocess
 import urllib
-
-# Scripts to set the desktop image. This only sets the current desktop.
-SET = """osascript -e 'tell application "Finder" to set desktop picture to POSIX file path'"""
-
-
-
-# Refresh the dock to ensure the wallpaper is set.
-REFRESH = """killall Dock"""
-
-
-# Calls the scipts to set the desktop and refresh the dock.
-def set_pic(path):
-    subprocess.Popen([SET, str(path)], shell=True)
-    subprocess.Popen(REFRESH, shell=True)
-
 
 # Define the reddit object.
 r = praw.Reddit(user_agent='User-Agent: osx:com.frocco.reddit_wallpaper:v0.1 (by /u/roclobster)')
 
-# Retrieve and save the top image of /r/wallpapers
+# Retrieve and save the top image of /r/WQHD_Wallpaper
 testfile = urllib.URLopener()
-#print list(r.get_subreddit('WQHD_Wallpaper').get_top(limit=1))[0].url
-testfile.retrieve(list(r.get_subreddit('wallpaper').get_top(limit=1))[0].url, "reddit_wallpaper.jpg")
+testfile.retrieve(list(r.get_subreddit('WQHD_Wallpaper').get_top(limit=1))[0].url, "reddit_wallpaper.jpg")
 
-print os.getcwd() + "/reddit_wallpaper.jpg"
+# Generate a fileURL for the desktop picture
+file_url = NSURL.fileURLWithPath_(os.getcwd() + "/reddit_wallpaper.jpg")
 
-# set the background using the absolute path of the image.
-set_pic(os.getcwd() + "/reddit_wallpaper.jpg")
+# Get shared workspace
+ws = NSWorkspace.sharedWorkspace()
 
+# Iterate over all screens
+for screen in NSScreen.screens():
+    # Tell the workspace to set the desktop picture
+    (result, error) = ws.setDesktopImageURL_forScreen_options_error_(file_url, screen, {}, None)
+    if error:
+        print error
+        exit(-1)
